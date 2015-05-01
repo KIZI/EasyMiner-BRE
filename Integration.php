e6865d768257cb190bf81d26a345f6e388aab5ab
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-05-01 16:36:58 +0200

    Added class for integration with EasyMinerCenter

--- Integration.php
+++ Integration.php
@@ -7,8 +7,15 @@ class Integration {
    * @var $javascriptFiles - this array contains list of javascript files for inclusion into page
    */
   public static $javascriptFiles = [
-    'js/jquery-ui-1.10.4.custom.min.css',
-    'js/jquery.apprise-v2.css'
+    'js/jquery-2.1.1.min.js',
+    'js/jquery-ui-1.10.4.custom.min.js',
+    'js/jstorage-0.4.8.min.js',
+    'js/jquery.i18n.min.js',
+    'js/jquery.json2.js',
+    'js/jquery.ui-contextmenu.js',
+    'js/jquery.apprise-v2.js',
+    'js/jquery.bre.js',
+    'js/jquery.bre.dragdrop.js'
   ];
 
   /**
