712ef190f9c3b2a12499b8956bc48380a5e38a35
Stanislav Vojíř <stanislav.vojir@gmail.com>
2015-05-01 16:36:12 +0200

    Added class for integration with EasyMinerCenter

---
+++ Integration.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace EasyMiner\BRE;
+
+class Integration {
+  /**
+   * @var $javascriptFiles - this array contains list of javascript files for inclusion into page
+   */
+  public static $javascriptFiles = [
+    'js/jquery-ui-1.10.4.custom.min.css',
+    'js/jquery.apprise-v2.css'
+  ];
+
+  /**
+   * @var $cssFiles - this array contains list of CSS files for inclusion into page
+   */
+  public static $cssFiles = [
+    'css/style.css'
+  ];
+
+}
